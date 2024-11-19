5c5e1629afb1e130f66a6a8b16eec784420fa69b
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2024-11-19 18:05:00 +0100

    Feat: Criado o primeiro arquivo function da pasta functionque retorna alguns elementos vazios

---
+++ src/core/evento/functions/criarEventoVazio.ts
@@ -0,0 +1,13 @@
+import { Id } from "@/core/shared";
+import { Evento } from "../model/Evento";
+
+export function CriarEventoVazio(): Partial<Evento> {
+  return {
+    id: Id.novo(),
+    nome: "",
+    descricao: "",
+    data: new Date(),
+    local: "",
+    publicoEsperado: 1,
+  };
+}
