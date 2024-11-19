ae0474c76fdfed0281e5ad65b74a798ec6c9e211
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2024-11-19 17:55:46 +0100

    Finalizado a criacao dos arquivos da pasta Shared, ou seja dos arquivos uteis

--- src/core/shared/index.ts
+++ src/core/shared/index.ts
@@ -1,5 +1,6 @@
 import { Alias } from "./alias";
 import { Id } from "./id";
 import { Senha } from "./senha";
+import { Data } from "./data";
 
-export { Alias, Id, Senha };
+export { Alias, Id, Senha, Data };
