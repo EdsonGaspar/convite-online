0dad2f610df3d378304f74b70fffd8790329d15b
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2024-11-19 13:10:16 +0100

    Feat: Criando e modelando a Core do projecto, no arquivo Evento, foi criando dois aquivos .ts e codificado nelas a suas interface.

--- src/components/template/pagina.tsx
+++ src/components/template/pagina.tsx
@@ -7,11 +7,7 @@ export interface PaginaProps {
 
 export function Pagina(props: PaginaProps) {
   return (
-    <div
-      className={
-        "flex flex-col items-center  py-10 min-h-screen bg-[url('/background.png')]  bg-cover"
-      }
-    >
+    <div className="flex flex-col items-center  py-10 min-h-screen bg-[url('/background.png')]  bg-cover">
       <Logo />
       <main
         className={`${props.clasName}  flex-1 flex flex-col justify-center bg-red-500 container`}
---
+++ src/core/evento/model/Evento.ts
@@ -0,0 +1,12 @@
+export interface Evento {
+  id: string; //UUID
+  alias: string; //aniversario-edson-2025
+  senha: string;
+  nome: string;
+  data: Date;
+  local: string;
+  descricao: string;
+  imagem: string;
+  imagemBackground: string;
+  publicoEsperado: number;
+}
