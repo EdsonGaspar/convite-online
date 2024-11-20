c071fd5853b85c46c1b6cb608ca7d4c635583b95
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2024-11-20 15:29:05 +0100

    Feat: Implementacao do Qr Code, e usando no seu Value um objecto assim gera um Qr Code para cada evento.

--- src/app/(dev)/eventos/page.tsx
+++ src/app/(dev)/eventos/page.tsx
@@ -1,6 +1,8 @@
-import { eventos } from "@/core";
+import { eventos, Senha } from "@/core";
 import Image from "next/image";
 import Link from "next/link";
+import { json } from "node:stream/consumers";
+import QRCode from "react-qr-code";
 
 export default function PaginaEvento() {
   return (
@@ -18,9 +20,17 @@ export default function PaginaEvento() {
               className="object-cover"
             />
           </div>
-          <div className="flex-1 flex flex-col gap-5 p-7">
-            <span className="text-lg font-black">{evento.nome}</span>
+          <div className="flex-1 flex flex-col items-center gap-5 p-7">
+            <span className="text-lg text-center font-black">
+              {evento.nome}
+            </span>
             <p className="flex-1 text-sm text-zinc-400">{evento.descricao}</p>
+
+            {/* Implementação do Qr-Code */}
+            <QRCode
+              value={JSON.stringify({ id: evento.id, senha: evento.senha })}
+              className="w-44 h-44"
+            />
 
             <div className="flex gap-5">
               <Link
