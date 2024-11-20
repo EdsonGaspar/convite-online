a1cd59bc531b74ff73035478cbda697cb93ffeee
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2024-11-20 13:31:50 +0100

    Feat: Criado a proposta da estrutora inicial do projecto e configuracao de varias corres do botao no arquivo global.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
-import { Pagina } from "@/components/template/pagina";
-import { Evento } from "@/core";
+import { Logo } from "@/components/template/logo";
+import Link from "next/link";
 
 export default function Home() {
-  const e: Partial<Evento> = {};
   return (
-    <Pagina clasName="w-full">
-      <div>Edson, dev 2024</div>
-    </Pagina>
+    <div className="bg-[url('/background-elementos.svg')] bg-cover h-screen flex flex-col items-center justify-center gap-10">
+      <div className="flex flex-col items-center gap-4">
+        <Logo />
+        <p className="text-zinc-500 font-light w-96 leading-6  text-center select-none">
+          Crie e gerencie o convite do seu evento de forma rápida e facíl, sem
+          complicações!
+        </p>
+      </div>
+      <Link href="/ecento" className="botao azul text-lg uppercase">
+        Criar seu Evento
+      </Link>
+    </div>
   );
 }
