c1d178728d7f2c3f48e08827cdc883446dabb15d
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2024-11-21 14:54:45 +0100

    Feat: Implentado as informacoes na Dashboard, tela grafica

--- src/components/evento/dashBoardEvento.tsx
+++ src/components/evento/dashBoardEvento.tsx
@@ -7,9 +7,9 @@ export interface DashBoardEventoProps {
 
 export default function DashBoardEvento(props: DashBoardEventoProps) {
   return (
-    <div>
-      <div>
-        <InformacoesEvento evento={props.evento} />
+    <div className="flex flex-col gap-2">
+      <div className="flex gap-2 ">
+        <InformacoesEvento evento={props.evento} className="flex-1" />
       </div>
     </div>
   );
