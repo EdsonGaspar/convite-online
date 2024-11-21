5d7b910107eff8f6539bb92b0c26201632c76520
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2024-11-21 15:30:13 +0100

    Feat: Implementacao do QrCode e usa configuracao na dashBord, apenas de uso para Admin

--- src/components/evento/dashBoardEvento.tsx
+++ src/components/evento/dashBoardEvento.tsx
@@ -1,5 +1,6 @@
 import { Evento, eventos } from "@/core";
 import InformacoesEvento from "./informacoesEventos";
+import { AcessarViaQrCode } from "./acessarViaQrCode";
 
 export interface DashBoardEventoProps {
   evento: Evento;
@@ -8,8 +9,9 @@ export interface DashBoardEventoProps {
 export default function DashBoardEvento(props: DashBoardEventoProps) {
   return (
     <div className="flex flex-col gap-2">
-      <div className="flex gap-2 ">
+      <div className="flex gap-2 self-stretch">
         <InformacoesEvento evento={props.evento} className="flex-1" />
+        <AcessarViaQrCode evento={props.evento} />
       </div>
     </div>
   );
